d87029ade19159dc36c3736a661e3ebdff02c87f
Olivia Nattrass <olivia.nattrass@psasquashtour.com>
2026-01-08 16:06:32 +0000

    adit articles controller

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,9 @@
 
 ActiveRecord::Schema[8.1].define(version: 2025_12_18_174724) do
   create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
     t.datetime "created_at", null: false
+    t.text "description"
+    t.string "title"
     t.datetime "updated_at", null: false
   end
 end
