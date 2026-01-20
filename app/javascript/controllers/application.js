32806e512f992e9741d369e080f7952c26f34ea6
Olivia Nattrass <olivia.nattrass@psasquashtour.com>
2026-01-20 16:02:58 +0000

    Homepage formatting

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,16 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+import "@popperjs/core";
+import "bootstrap";
+
+document.addEventListener("turbo:load", function () {
+  // This code is copied from Bootstrap's docs.
+  var tooltipTriggerList = [].slice.call(
+    document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  );
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+});
